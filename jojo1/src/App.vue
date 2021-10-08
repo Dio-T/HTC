ea17ceea79e09e08aeab9b038ef04a3036453a11
黄天成 <565447718@qq.com>
2021-10-08 21:08:42 +0800

    测试测试

---
+++ jojo1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="H">
+    <div class="T">
+      <p class="C">5</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+}
+.H{
+  width: 100%;
+  height: 500px;
+  background: rgb(255, 153, 153);
+  position: relative;
+}
+.T{
+  position: absolute;
+  height: 100px;
+  top: 0;
+  bottom: 0;
+  padding: 0;
+  margin: auto;
+  left: 0;
+  background: chartreuse;
+  display: block
+}
+.C{
+   text-align: center;
+   position: relative;
+     font-size: 50px;
+}
+</style>
